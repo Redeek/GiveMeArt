04049236cc14ea34f629231c58fe3011998b131b
Redeek <mati.red@gmail.com>
2023-08-20 18:35:02 +0200

    created ProgressBar

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Header} from './components/Header'
+import Logo from './components/Logo';
+import ProgressBar from './components/ProgressBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Logo />
+      <ProgressBar />
     </div>
   );
 }
---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const ProgressBar = () => {
+  return (
+    <div>
+        <div className='progressBar'>
+            <div className='circle active'>1</div>
+            <div className='circle unactive'>2</div>
+            <div className='circle unactive'>3</div>
+        </div>
+        <Row style={{marginLeft:"5%", marginRight:"5%", marginTop:"50px"}}>
+            <Col className='progressBarText activeText'>Generate image</Col>
+            <Col className='progressBarText unactiveText'>Choose your product</Col>
+            <Col className='progressBarText unactiveText' style={{}}>Order</Col>
+        </Row>
+    </div>)
+}
+
+export default ProgressBar
